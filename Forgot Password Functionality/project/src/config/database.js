bc1746d6d97263bb221217fb0272877182774992
Sania Nisar <102464125+SaniaNisar@users.noreply.github.com>
2024-08-11 12:27:53 +0500

    Add files via upload

---
+++ Forgot Password Functionality/project/src/config/database.js
@@ -0,0 +1,22 @@
+// require('dotenv').config(); 
+
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false
+// });
+
+// module.exports = sequelize;
+
+import 'dotenv/config'; // Use this to load environment variables
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging: false
+});
+
+export default sequelize;
